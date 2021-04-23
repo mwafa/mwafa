1d179036d08664a4a30a02c36f17749bdd87dee6
Muhammad Wafa <wafax.4@gmail.com>
2021-04-24 06:21:10 +0700

    initial nextra

---
+++ pages/index.md
@@ -0,0 +1,15 @@
+# Hi 👋, I'm wafa
+
+Selamat datang di blog pribadi saya,
+dalam blog ini saya akan mencoba membagikan beberapa artikel terkait pemrograman secara reguler.
+Semoga kamu bisa menikmati setiap konten yang tersedia dan mendapatkan manfaat.
+
+**Terimakasih kepada**:
+
+> **github** - yang telah menyediakan penyimpanan repositori dan berbagai fasilitas lain.\
+> **vercel** - yang telah menyediakan hosting gratis, framework dan template blog ini.
+
+## Profil Github
+
+[![https://gpc.znoo.xyz/api/github/mwafa](https://gpc.znoo.xyz/api/github/mwafa)
+](https://github.com/mwafa)
---
+++ theme.config.js
@@ -0,0 +1,30 @@
+export default {
+  repository: "https://github.com/mwafa/mwafa",
+  docsRepository: "https://github.com/mwafa/mwafa",
+  branch: "master",
+  path: "/",
+  titleSuffix: " – mwafa",
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null,
+  darkMode: true,
+  footer: true,
+  footerText: `${new Date().getFullYear()} © mwafa.net`,
+  footerEditOnGitHubLink: false,
+  logo: (
+    <>
+      <span>MWAFA</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="I am Wafa, passionate to build something."
+      />
+      <meta name="og:title" content="Muhammad Wafa" />
+    </>
+  ),
+}
