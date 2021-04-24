3d93bb295f9c95eb30687d0d412f1ae815a5c9d8
Muhammad Wafa <wafax.4@gmail.com>
2021-04-24 07:21:55 +0700

    fix: webpack 5

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withNextra = require("nextra")("nextra-theme-docs", "./theme.config.js")
-module.exports = withNextra()
+
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  ...withNextra(),
+}
